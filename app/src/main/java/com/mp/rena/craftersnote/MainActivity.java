05efe59b60c319bc69163beef594d7ac3c7caf28
reinal <ff14purinnnn@gmail.com>
2018-12-24 17:45:16 -0800

    Added fragments and basic structure

--- app/src/main/java/com/mp/rena/craftersnote/MainActivity.java
+++ app/src/main/java/com/mp/rena/craftersnote/MainActivity.java
@@ -1,13 +1,57 @@
 package com.mp.rena.craftersnote;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigation;
+    private FragmentManager fragmentManager;
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.inflateMenu(R.menu.bottom_menu);
+        fragmentManager = getSupportFragmentManager();
+
+        fragment = new TodaysTask();
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_container, fragment).commit();
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.action_today:
+                        fragment = new TodaysTask();
+                        break;
+                    case R.id.action_task:
+                        fragment = new ManageTask();
+                        break;
+                    case R.id.action_gather:
+                        fragment = new ManageGather();
+                        break;
+
+                    case R.id.action_record:
+                        fragment = new RecordSales();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container, fragment).commit();
+                return true;
+            }
+        });
+
+
     }
 }
